829d9e73902a01d135cda47c96682e96e2e71c4d
Marcelo Pereira <marceloleandropereira@gmail.com>
2022-02-08 23:32:03 -0300

    feat(names-list): adds a new component with names list and imports for module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NamesComponent } from './names/names.component';
+import { NamesListComponent } from './names/names-list/names-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NamesComponent,
+    NamesListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/names/names-list/names-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-names-list',
+  templateUrl: './names-list.component.html',
+  styleUrls: ['./names-list.component.scss']
+})
+export class NamesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
