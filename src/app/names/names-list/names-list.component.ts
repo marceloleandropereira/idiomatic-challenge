80f298549879f3f54e72735207273645969ff8ae
Marcelo Pereira <marceloleandropereira@gmail.com>
2022-02-10 00:51:18 -0300

    feat(names-list): isolate name listing in a component

--- src/app/names/names-list/names-list.component.ts
+++ src/app/names/names-list/names-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FullNameStorage } from '../models/full-name.model';
 
 @Component({
   selector: 'app-names-list',
   templateUrl: './names-list.component.html',
   styleUrls: ['./names-list.component.scss']
 })
-export class NamesListComponent implements OnInit {
+export class NamesListComponent {
+  
+  @Input() public fullNames: FullNameStorage[];
+  @Input() public indexSelectedName: number;
+  
+  @Output() public onSelectName: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public selectName(index: number): void {
+    this.onSelectName.emit(index);
   }
-
 }
